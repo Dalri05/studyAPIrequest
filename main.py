2c5e6ea6fd1a37f3b8fd6fd8d0e757bfff33d935
João Dalri <105470028+Dalri05@users.noreply.github.com>
2024-03-25 17:00:19 -0300

    Adicionado funcoes de userid e conexao com SQL

--- main.py
+++ main.py
@@ -1,13 +1,41 @@
+import mysql.connector
 from fastapi import FastAPI
-from pydantic import BaseModel
 
 app = FastAPI()
 
-class User(BaseModel):
-    name: str
-    age: int
+try:
+    conexao = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="123456",
+        database="pessoas"
+    )
+    cursor = conexao.cursor()
+except mysql.connector.Error as err:
+    print(f"Erro de conexão com o banco de dados: {err}")
+    exit(1)
 
-@app.get('/user', response_model=User)
+class comandos:
+    def readuser(idnome):
+        comando = 'SELECT * FROM pessoas.usuarios WHERE idnome=%s' 
+        cursor.execute(comando, (idnome,))
+        resultado = cursor.fetchall()
+        return resultado
+
+    def read():
+        comando = 'SELECT * FROM pessoas.usuarios'
+        cursor.execute(comando)
+        resultado = cursor.fetchall()
+        return resultado
+
+@app.get('/user')
 def get_user():
-    user_data = {'name': 'Fulano', 'age': 30}
-    return User(**user_data)
+    return comandos.read()
+
+@app.get('/user/{idnome}')
+def get_userid(idnome:int):
+    return comandos.readuser(idnome)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8002)
